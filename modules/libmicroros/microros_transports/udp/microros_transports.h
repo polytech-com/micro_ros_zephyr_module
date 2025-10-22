ec51cea15843372cea78e04094901f52e9341c56
Anders Bjørn Nedergaard <abn@polytech.com>
2025-10-22 20:25:33 +0200

    support zephyr > 3.1.0

--- modules/libmicroros/microros_transports/udp/microros_transports.h
+++ modules/libmicroros/microros_transports/udp/microros_transports.h
@@ -18,9 +18,15 @@
 #include <unistd.h>
 
 #include <sys/types.h>
+#include <zephyr/version.h>
+#if ZEPHYR_VERSION_CODE >= ZEPHYR_VERSION(3, 1, 0)
+#include <zephyr/posix/sys/socket.h>
+#include <zephyr/posix/poll.h>
+#else
 #include <posix/sys/socket.h>
 #include <posix/poll.h>
 
+#endif
 #ifdef __cplusplus
 extern "C"
 {
